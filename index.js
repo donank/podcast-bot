9834283cceee643bea94919104228368c643a0e9
donank <anandankit@protonmail.com>
2019-05-14 00:39:26 +0530

    basic commands

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const {
 	prefix,
 	token,
 } = require('./config.json');
+
+const { podcasts } = require('./podcasts.json')
+
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
@@ -20,7 +23,9 @@ client.once('reconnecting', () => {
 client.once('disconnect', () => {
 	console.log('Disconnect!');
 });
-client.login(token);
+
+var flag = false
+var choice = 1
 client.on('message', message => {
     console.log("triggered")
     if(!message.guild) return;
@@ -39,4 +44,64 @@ client.on('message', message => {
             message.reply("You need to join a voice channel first!");
         }
     }
+    if (message.content === `${prefix}list`) {
+        console.log("list command triggered")
+        const embed = new Discord.RichEmbed()
+        .setColor('#8700a2')
+        .setTitle('Topics')
+        .setDescription('Select the topic by entering \"o!select <number>\"')
+        .addField('1. Politics', 'Placeholder')
+        .addField('2. Economics', 'PlacedHolder')
+        .addField('3. Policy', 'PlaceHolder')
+        .addField('4. Law', 'PlaceHolder')
+        .addField('5. History', 'PLaceHolder')
+
+        message.channel.send(embed)
+        flag = true
+    }
+    if (message.content === `${prefix}select`) {
+        console.log("Select choice command triggered")
+        if(flag == true){
+            switch(choice){
+                case 1:{
+                    console.log("1 triggered")
+                    var pol_map = [];
+                    var pol_map = politics.map((element, index) => (
+                        {
+                            name: (index+1)+ ". "+element.name, 
+                            value: element.link
+                        }
+                    ));
+                    console.log(pol_map)
+                    const subEmbed = {
+                        color: 0x8700a2,
+                        title: 'Politics',
+                        description: 'Select the audio by entering \"o!play <number>\"',
+                        fields: pol_map
+                    };
+                    message.channel.send({embed: subEmbed})
+                }
+                case 2:{
+                    
+                }
+                case 3:{
+    
+                }
+                case 4:{
+    
+                }
+                case 5:{
+    
+                }
+            }
+        }
+    }
+
+    if(message.content === `${prefix}add`+` `+`${topic}`){
+
+    }
 });
+
+client.login(token);
+
+
